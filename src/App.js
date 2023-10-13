fe5ba675b06c1713790cd05094b110e41dff6a1a
Munjerin Hossain <munjerin15muna@gmail.com>
2023-10-13 12:32:54 +1100

    created Home page partially

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AllPost from './AllPost.js'
+import Home from "./Home.js"
 
 function App() {
   return (
     <Router>
       <div className="App">
 
-        <Route path="/" component={AllPost} />
+        <Route path="/" component={Home} />
        
        
       </div>
