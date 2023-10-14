77c403850cfe5b27210561d3232016c00781c0ef
Munjerin Hossain <munjerin15muna@gmail.com>
2023-10-14 17:07:31 +1100

    implemented login, signup button with title

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
 
         <Route path="/" component={Home} />
+        <Route path="/profile" component={AllPost} />
        
        
       </div>
--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,17 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import HomeImg from './assests/home.jpg'
+import AllPost from './AllPost';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    //display: 'flex',
     
   },
   menuButton: {
@@ -18,12 +25,27 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 152,
+  },
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
 
+  // const handleLogin =() =>{
+  //   Navigation.navigate('AllPost')
+  // }
+
   return (
+    <>
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
@@ -40,6 +62,29 @@ export default function ButtonAppBar() {
           <Button color="inherit">About Us</Button>
         </Toolbar>
       </AppBar>
+     
+      <Card style={{height: '800px', width:'750px'}}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+          Task Management System
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+          One Platform
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+          Manage Your Work
+          </Typography>
+          <Button variant="contained" color="primary" style={{margin:5}}>Sign Up</Button>
+          <Button variant="contained" color="primary" >Login</Button>
+          {/* <CardMedia className={classes.cover} image={HomeImg} title="TMS Home"/> */}
+        </CardContent>  
+
+          </div>
+        
+      </Card>
+ 
     </div>
+      </>
   );
 }
