462328e0ec253fb113bfacefbf93f97b184cdd4b
Munjerin Hossain <munjerin15muna@gmail.com>
2023-10-20 12:56:04 +1100

    fixed dashboard UI.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -26,11 +26,14 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: theme.palette.text.secondary,
     whiteSpace: "nowrap",
-    height: 650,
+    height: 600,
     width: 200,
     borderRadius: 40,
-    marginLeft: 20,
+    marginLeft: 40,
     marginTop: 20,
+    bottom: 20,
+    position:'absolute',
+    display:'flex'
   },
   avatar: {
     color: "black",
@@ -48,74 +51,103 @@ export default function CenteredGrid() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <Grid
-              item
-              xs={6}
-              style={{
-                width: 200,
-                alignContent: "space-around",
-                alignItems: "center",
-              }}
-            >
-              <Avatar className={classes.avatar}>MH</Avatar>
-              <MenuList style={{ top: 20 }}>
-                <div style={{ display: "grid" }}>
-                  <Button
-                    variant="contained"
-                    style={{ borderRadius: 40, marginTop: 15, left: 34 }}
-                  >
-                    My Profile
-                  </Button>
-                  <Link to='/allTasks'>
-                  <Button
-                    variant="contained"
-                    style={{ borderRadius: 40, marginTop: 15, left: 34 }}
-                  >
-                    My Task
-                  </Button>
-                  </Link>
-                  <Link to="/allProject">
+    <>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Grid
+                item
+                xs={6}
+                style={{
+                  width: 220,
+                  alignContent: "space-around",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar className={classes.avatar}>MH</Avatar>
+                <MenuList style={{ top: 20 }}>
+                  <div style={{ display: "grid"}}>
                     <Button
                       variant="contained"
                       style={{ borderRadius: 40, marginTop: 15, left: 34 }}
                     >
-                      My Project
+                      My Profile
                     </Button>
-                  </Link>
-                  <Button
-                    variant="contained"
-                    style={{ borderRadius: 40, marginTop: 15, left: 34 }}
-                  >
-                    Meeting
-                  </Button>
-                  <Button
-                    variant="contained"
-                    style={{ borderRadius: 40, marginTop: 15, left: 34 }}
-                  >
-                    Timeline
-                  </Button>
-                  <Button
-                    variant="contained"
-                    style={{ borderRadius: 40, marginTop: 15, left: 34 }}
-                  >
-                    Settings
-                  </Button>
-                  <Button
-                    variant="contained"
-                    style={{ borderRadius: 40, marginTop: 15, left: 34, top:10 }}
-                  >
-                    Logout
-                  </Button>
-                </div>
-              </MenuList>
-            </Grid>
+                    <Link to="/allTasks">
+                      <Button
+                        variant="contained"
+                        style={{ borderRadius: 40, marginTop: 15, left: 34 }}
+                      >
+                        My Task
+                      </Button>
+                    </Link>
+                    <Link to="/allProject">
+                      <Button
+                        variant="contained"
+                        style={{ borderRadius: 40, marginTop: 15, left: 34 }}
+                      >
+                        My Project
+                      </Button>
+                    </Link>
+                    <Button
+                      variant="contained"
+                      style={{ borderRadius: 40, marginTop: 15, left: 34 }}
+                    >
+                      Meeting
+                    </Button>
+                    <Button
+                      variant="contained"
+                      style={{ borderRadius: 40, marginTop: 15, left: 34 }}
+                    >
+                      Timeline
+                    </Button>
+                    <Button
+                      variant="contained"
+                      style={{ borderRadius: 40, marginTop: 15, left: 34 }}
+                    >
+                      Settings
+                    </Button>
+                    <Link to="/">
+                      <Button
+                        variant="contained"
+                        style={{
+                          borderRadius: 40,
+                          marginTop: 15,
+                          left: 34,
+                          top: 10,
+                        }}
+                      >
+                        Logout
+                      </Button>
+                    </Link>
+                  </div>
+                </MenuList>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item xs={8} style={{alignContent:'center', marginLeft:360}}>
+          <Paper style={{height: 600, borderRadius:80 }}>
+            <div style={{  marginTop:30 }}>
+              <Typography variant="h6" component="h2" style={{fontSize:22, fontWeight:'bold', color:'#808080', paddingTop:80}}>
+                Dashboard is on progress...
+              </Typography>
+            </div>
+            {/* <Grid
+             item
+             xs={6}
+             style={{
+               width: 200,
+               alignContent: "space-around",
+               alignItems: "center",
+             }}
+           >
+           
+           </Grid> */}
           </Paper>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+    </>
   );
 }
